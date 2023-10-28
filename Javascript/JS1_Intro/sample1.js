33bcf626e3fef055fe40f5f7a2864b26bd6c5fcd
vmurali100 <vmurali100@users.noreply.github.com>
2023-10-28 15:06:48 +0530

    added

--- Javascript/JS1_Intro/sample1.js
+++ Javascript/JS1_Intro/sample1.js
@@ -1,1 +1,23 @@
-alert("Welcome to Javascript !! .. Have enjoyble Learing ")
+// alert("Welcome to Javascript !! .. Have enjoyble Learing ")
+
+var myName = "Ram Krishna";
+var myAge = 40;
+var isAvaiable = false;
+var propertyWorth = null;
+var someInfo;
+
+// document.write(myName+"<br/>");
+// document.write(myAge+"<br/>")
+// document.write(isAvaiable+"<br/>")
+// document.write(propertyWorth+"<br/>[=[")
+// document.write(someInfo)
+
+var person = {
+    fname:"Murali",
+    lname:"Krishna",
+    email:"murali@gmail.com"
+}
+
+document.write(person.fname+"<br/>");
+document.write(person.lname+"<br/>")
+document.write(person.email)
