8e9306d384427d166124e14b3c693ca2d0a9fef8
smswapna924 <smswapna924@gmail.com>
2023-10-30 19:54:05 +0530

    added

---
+++ HTML 3.DIV/HTML 2/HTML 5/sample2.js
@@ -0,0 +1,33 @@
+// ------variables----
+var myname ="swapna";
+var myage = 21;
+var myearth = false;
+var property = null;
+var someinfo;
+
+document.write(myname+"<br/>");
+document.write(myage+"<br/>");
+document.write(myearth+"<br/>");
+document.write(property+"<br/>");
+document.write(someinfo+"<br/>");
+
+// ----objects-----
+var person ={
+    fname:"m.swapna",
+    lname:"swapna",
+    email:"smswapna@gmail.com",
+    address:"Dalavaipalli",
+    mandal:"yadamari",
+    number:"9014870388",
+    street:"chittoor",
+    pincode:"512200",
+}
+
+document.write(person.fname+"<br/>");
+document.write(person.lname+"<br/>");
+document.write(person.email+"<br/>");
+document.write(person.address+"<br/>");
+document.write(person.mandal+"<br/>");
+document.write(person.number+"<br/>");
+document.write(person.street+"<br/>");
+document.write(person.pincode+"<br/>");
---
+++ HTML 3.DIV/HTML 2/HTML 5/sample3.js
@@ -0,0 +1,46 @@
+// -------variables------
+var myname ="m.naveenkumar";
+var myage =24;
+var iamgoodboy =true;
+var mybankbalance =null;
+var someinformation =undefined;
+
+document.write(myname+"<br/>");
+document.write(myage+"<br/>");
+document.write(iamgoodboy+"<br/>");
+document.write(mybankbalance+"<br/>");
+document.write(someinformation+"<br/>");
+
+// ----objrcts------
+var person ={
+    fname:"m.naveenkumar",
+    lname:"naveen",
+    myage:"24",
+    Gender:"male",
+    mobilenumber:"7993748057",
+    Education:"Degree",
+    Email:"naveenkumar1234@gmail.com",
+    address:"banglore",
+    presentworking:"autodriver",
+    languages:"Telugu,English,kanada,tamil",
+    pincode:"1234567",
+    salary:"expected",
+    state:"Andrapradesh",
+    Nativeplace:"Dalavaipalli",
+
+}
+
+document.write(person.fname+"<br/>");
+document.write(person.lname+"<br/>");
+document.write(person.myage+"<br/>");
+document.write(person.Gemder+"<br/>");
+document.write(person.mobilenumber+"<br/>");
+document.write(person.Education+"<br/>");
+document.write(person.Email+"<br/>");
+document.write(person.address+"<br/>");
+document.write(person.presentworking+"<br/>");
+document.write(person.languages+"<br/>");
+document.write(person.pincode+"<br/>");
+document.write(person.salary+"<br/>");
+document.write(person.state+"<br/>");
+document.write(person.Nativeplace+"<br/>");
