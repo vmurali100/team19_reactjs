68dab06df897ca5591e5ddae2739d7e102e633c3
smswapna924 <smswapna924@gmail.com>
2023-11-01 20:09:18 +0530

    added

---
+++ HTML 3.DIV/HTML 2/HTML 5/sample4.js
@@ -0,0 +1,29 @@
+// -------variables----
+var myname = "Bhuvaneswari";
+var age =43;
+var myearth =true;
+var property =null;
+var somecontent=undefined;
+
+document.write(myname+"<br/>");
+document.write(age+"<br/>");
+document.write(myearth+"<br/>");
+document.write(property+"<br/>");
+document.write(somecontent+"<br/>");
+
+// --------objects-----
+var person= {
+    fname:"G.lokesh",
+    lname:"lokesh",
+    email:"lokeshlokesh@gmail.com",
+    mobilenumber:"6303394841",
+    address:"perimalapalli",
+    Gender:"male",
+}
+
+document.write(person.fname+"<br/>");
+document.write(person.lname+"<br/>");
+document.write(person.email+"<br/>");
+document.write(person.mobilenumber+"<br/>");
+document.write(person.address+"<br/>");
+document.write(person.Gender+"<br/>");
