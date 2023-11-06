44316a04c9a55c85266a5552b6ae6e2e8cde13fb
mdileep944 <mdilep944@gmail.com>
2023-11-06 13:10:17 +0530

    add

---
+++ LAYOUTS/sample1.js/sam1.js
@@ -0,0 +1,21 @@
+var myname = "Dillep";
+var myage = 40;
+var live = true;
+var property = null;
+var someinfo;
+
+// document.write(myname+"<br/>");
+// document.write(myage+"<br/>");
+// document.write(live+"<br/>");
+// document.write(property+"<br/>");
+// document.write(someinfo+"<br/>");
+
+var person = {
+    firstname:"Dileep",
+    lastname:"Reddy",
+    email:"dileep.mekala@944gmail.com"
+}
+
+document.write(person.firstname+"<br/>")
+document.write(person.lastname+"<br/>")
+document.write(person.email+"<br/>")
---
+++ LAYOUTS/sample1.js/sam2.js
@@ -0,0 +1,22 @@
+var myname = "VASU";
+var myage = 30;
+var live = false;
+var property = null;
+var someinfo;
+
+document.write(myname+"<br/>");
+document.write(myage+"<br/>");
+document.write(live+"<br/>");
+document.write(property+"<br/>");
+document.write(someinfo+"<br/>");  
+
+
+var person = {
+    firstname:"VASU",
+    lastname:"Reddy",
+    email:"vasu899u@gmail.com"
+}
+
+document.write(person.firstname+"<br/>")
+document.write(person.lastname+"<br/>")
+document.write(person.email+"<br/>")
