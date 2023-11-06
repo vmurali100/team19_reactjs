281569b1c498f7c004a874d9db44cfc187ee24d6
smswapna924 <smswapna924@gmail.com>
2023-11-06 11:11:01 +0530

    added

---
+++ HTML 3.DIV/HTML 2/HTML 5/sample9.js
@@ -0,0 +1,28 @@
+// ------variables----
+var myname="m.swapna";
+var age=21;
+var property=false;
+var bankbalance=null;
+var someinfo=undefined;
+
+document.write(myname+"<br/>");
+document.write(age+"<br/>");
+document.write(property+"<br/>");
+document.write(bankbalance+"<br/>");
+document.write(someinfo+"<br/>");
+
+
+// -----Objects-----
+var person={
+    fname:"m.swapna",
+    lname:"swapna",
+    Gender:"female",
+    number:"1234567891",
+    email:"smswapna@gmail.com",
+}
+
+document.write(person.fname+"<br/>");
+document.write(person.lname+"<br/>");
+document.write(person.Gender+"<br/>");
+document.write(person.number+"<br/>");
+document.write(person.email+"<br/>");
