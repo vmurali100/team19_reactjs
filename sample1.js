e8c082a7cf34f7e6e1e4ae82abc26c94790fc4f0
matavalamhimadeepredy <matavalamhimadepreddy@gmail.com>
2023-11-14 09:32:58 +0530

    deleted all files here

---
+++ sample1.js
@@ -0,0 +1,21 @@
+//var myName = "himadeep";
+//var myAge = 23;
+//var isAvaiable = false;
+//var propertyWorth = null;
+//var someInfo;
+
+// document.write(myName+"<br/>");
+ //document.write(myAge+"<br/>");
+ //document.write(isAvaiable+"<br/>")
+ //document.write(propertyWorth+"<br/>");
+ //document.write(someInfo);
+
+var person = {
+    fname:"hima",    
+        lname:"reddy",
+    email:"hima125@gmail.com",
+}
+
+document.write(person.fname+"<br/>");
+document.write(person.lname+"<br/>");
+document.write(person.email+"<br/>");
