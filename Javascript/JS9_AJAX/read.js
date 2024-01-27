4f19fe67854bf0dc3c2195d2c2f3fed7e5578012
vmurali100 <vmurali100@users.noreply.github.com>
2024-01-27 17:06:09 +0530

    Added BOM & Fetch Method code

--- Javascript/JS9_AJAX/read.js
+++ Javascript/JS9_AJAX/read.js
@@ -1,43 +1,42 @@
-
-var users = []
-function getUsersFromAPI(){
-    var getData = new XMLHttpRequest();
-    getData.onreadystatechange = function (){
-        if(getData.readyState === 4 && getData.status === 200){
-            console.log("User added",JSON.parse(getData.response))
-            users = JSON.parse(getData.response);
-            displayUsers()
-        }
+var users = [];
+function getUsersFromAPI() {
+  var getData = new XMLHttpRequest();
+  getData.onreadystatechange = function () {
+    if (getData.readyState === 4 && getData.status === 200) {
+      console.log("User added", JSON.parse(getData.response));
+      users = JSON.parse(getData.response);
+      displayUsers();
     }
-    getData.open("GET","http://localhost:3000/users");
-    getData.send()
+  };
+  getData.open("GET", "http://localhost:3000/users");
+  getData.send();
 }
-getUsersFromAPI()
+getUsersFromAPI();
 
-function displayUsers(){
-    console.log(users)
-    for (let i = 0; i < users.length; i++) {
-       var myTr = document.createElement("tr")
-       var eachObj = users[i]
-       for(a in eachObj){
-        var myTd = document.createElement('td');
-        myTd.innerHTML = eachObj[a]
-        myTr.appendChild(myTd)
-       }
-
-       var editTd = document.createElement("td");
-       var deleteTd = document.createElement("td");
-       
-       var editBtn = document.createElement("button");
-       editBtn.innerHTML ="Edit"
-       editBtn.setAttribute("onclick","editUser("+i+")")
-       var deleteBtn = document.createElement("button");
-       deleteBtn.innerHTML="Delete"
-       deleteBtn.setAttribute("onclick","deleteUser("+i+")")
-       editTd.appendChild(editBtn);
-       deleteTd.appendChild(deleteBtn)
-       myTr.appendChild(editTd);
-       myTr.appendChild(deleteTd)
-        document.querySelector("tbody").appendChild(myTr)
+function displayUsers() {
+  console.log(users);
+  for (let i = 0; i < users.length; i++) {
+    var myTr = document.createElement("tr");
+    var eachObj = users[i];
+    for (a in eachObj) {
+      var myTd = document.createElement("td");
+      myTd.innerHTML = eachObj[a];
+      myTr.appendChild(myTd);
     }
+
+    var editTd = document.createElement("td");
+    var deleteTd = document.createElement("td");
+
+    var editBtn = document.createElement("button");
+    editBtn.innerHTML = "Edit";
+    editBtn.setAttribute("onclick", "editUser(" + i + ")");
+    var deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "Delete";
+    deleteBtn.setAttribute("onclick", "deleteUser(" + i + ")");
+    editTd.appendChild(editBtn);
+    deleteTd.appendChild(deleteBtn);
+    myTr.appendChild(editTd);
+    myTr.appendChild(deleteTd);
+    document.querySelector("tbody").appendChild(myTr);
+  }
 }
