705a328a8440059fb49086f8343ed34ae25072bc
JayasankarRaju <rajubhai7341@.com>
2024-01-30 13:19:53 +0530

    adding updated code

--- Project/ReadUser.js
+++ Project/ReadUser.js
@@ -1,10 +1,1 @@
-// function readUser(e){
-//     e.preventDefault();
-//     var user={
-//         fname:document.getElementById("fname").value,
-//         email:document.getElementById("email").value,
-//         state:document.getElementById("state").value,
-//         dob:document.getElementById("dob").value,
-//         subjects:[]
-//     }
-//     console.log(user)
+
