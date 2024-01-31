e046684caef756dcb2db98f4c3cf47cefaa06152
smswapna924 <smswapna924@gmail.com>
2024-01-31 14:54:55 +0530

    added

---
+++ HTML 3.DIV/JS10_BOM/create.js
@@ -0,0 +1,30 @@
+
+function readUsersFromForm(){
+    var user = {
+        fname: document.getElementById("fname").value,
+        email: document.getElementById("email").value,
+        gender: document.getElementById("gender").value,
+        state: document.getElementById("state").value,
+        subjects: [],
+      };
+      var allSubjects = document.getElementsByName("subjects");
+      for (let index = 0; index < allSubjects.length; index++) {
+        if (allSubjects[index].checked) {
+          user.subjects.push(allSubjects[index].value);
+        }
+      }
+
+      return user
+}
+function createUser() {
+    var user = readUsersFromForm()
+  
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: { "Content-Type": "application/json" },
+  }).then(()=>{
+    getUsersFromAPI()
+  })
+
+}
