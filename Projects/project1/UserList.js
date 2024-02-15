933e516c515087adaee248258c8e621829c7b99b
JayasankarRaju <rajubhai7341@.com>
2024-02-15 16:21:12 +0530

    new updated code

--- Projects/project1/UserList.js
+++ Projects/project1/UserList.js
@@ -1,6 +1,7 @@
 async function GetData() {
     var response = await fetch("http://localhost:3000/Users")
     data = await response.json()
+  
 
     data.forEach(element => {      
         var mytr = document.createElement("tr")
@@ -9,6 +10,11 @@ async function GetData() {
             mytr.appendChild(myTd)
             myTd.innerHTML = element[a]
         }
+        var editTd=document.createElement("td")         
+        mytr.appendChild(editTd)
+        var editBtn= document.createElement("button")
+        editBtn.innerHTML="Edit"
+        editTd.appendChild(editBtn)
         document.querySelector("tbody").appendChild(mytr)
     });
 }
