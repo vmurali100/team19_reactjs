9c2320ea446cce61b1bd8ecf688070c33c39a993
Gnanasekar03 <138458161+Gnanasekar03@users.noreply.github.com>
2024-02-22 21:20:00 +0530

    Created New React app &  added

---
+++ ReactJs/app1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Employee, { User } from './Employee';
+
+function App() {
+  return (
+    <div className="App">
+      <Employee/>
+      <User/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJs/app1/src/Employee.jsx
@@ -0,0 +1,7 @@
+export default function Employee(){
+    return <h1>Welcome To Employee Componenet...!!!</h1>
+}
+
+export function User(){
+    return <h1>Welcome To User Componenet...!!! </h1>
+}
