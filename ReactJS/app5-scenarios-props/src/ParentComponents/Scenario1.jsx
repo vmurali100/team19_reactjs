c0fa3e5dcb89e6c53605841406588f851ff04b33
chandrakaladande <chandrakaladande1505@gmail.com>
2024-03-02 12:09:35 +0530

    Added

--- ReactJS/app5-scenarios-props/src/ParentComponents/Scenario1.jsx
+++ ReactJS/app5-scenarios-props/src/ParentComponents/Scenario1.jsx
@@ -8,6 +8,7 @@ const[message, setMessage] = useState("Hello World");
 return(
 
     <div>
+          <h2 style={{color:"blue"}}>Scenarios Using Props</h2><hr />
           <h2 style={{color: "green"}}>Scenario 1</h2>
           <NewMessage newMsg = {message}/>
           <hr />
