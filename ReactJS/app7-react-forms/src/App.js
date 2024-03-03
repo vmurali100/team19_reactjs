47ca7e2b010632cb0bbfd4ada48fd76af114f19c
chandrakaladande <chandrakaladande1505@gmail.com>
2024-03-03 21:59:31 +0530

    Added Crud Functional Comp

---
+++ ReactJS/app7-react-forms/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { UserCrud } from './Components/UserCrud';
+
+function App() {
+  return (
+    <div className="App">
+    <UserCrud/>
+    </div>
+  );
+}
+
+export default App;
