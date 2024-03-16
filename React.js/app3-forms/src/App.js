2b133dee00623c17d6daf67c26f632263fb1af9b
esambattupavan25 <esambattupavan25@gmail.c0m>
2024-03-16 08:48:13 +0530

    Usercrud practice

---
+++ React.js/app3-forms/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Reactform from './Crudapplications/Sample1_ReactForm';
+import Usercrud1 from './Crudapplications/USERCRUD1';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Reactform/> */}
+      <Usercrud1/>
+    </div>
+  );
+}
+
+export default App;
